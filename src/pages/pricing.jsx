4f7e1d98875f32befa4b72280e3b4b0a245f6138
v-magiri <ndung'u.victor19@students.dkut.ac.ke>
2024-02-28 21:35:24 +0300

    Add Z Coding

---
+++ src/pages/pricing.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const PricingComponent = () => {
+  return (
+    <div>
+      <h4>
+        <p></p>
+        <p></p>
+        <p></p>
+        <p></p>
+      </h4>
+    </div>
+  )
+}
+
+export default PricingComponent
